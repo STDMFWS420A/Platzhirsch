e63140ed4b72ec111d725641eec23bea9716a4b4
Jens <JensEsser2@t-online.de>
2023-02-24 11:09:58 +0100

    implement Server Implement Tests (Harness, HealthCheck)

--- src/main/java/hochschule/Main.java
+++ src/main/java/hochschule/Main.java
@@ -1,7 +1,26 @@
 package hochschule;
+import java.net.URISyntaxException;
+import java.net.URL;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
-    public static void main(String[]args){
-    System.out.println("Heyo");
+    public static void main(String[]args) throws Exception {
+        // Create a server that listens on port 8080.
+        Server server = new Server(5000);
+        WebAppContext webAppContext = new WebAppContext();
+        server.setHandler(webAppContext);
+
+        // Load static content from the top level directory.
+        URL webAppDir = Main.class.getClassLoader().getResource(".");
+        webAppContext.setResourceBase(webAppDir.toURI().toString());
+
+        // Start the server! 🚀
+        server.start();
+        System.out.println("Server started!");
+
+        // Keep the main thread alive while the server is running.
+        server.join();
+
     }
 }
--- src/test/java/hochschule/MainTest.java
+++ src/test/java/hochschule/MainTest.java
@@ -8,22 +8,39 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.function.Consumer;
 
 public class MainTest extends TestCase {
-
-    public void testMain() throws URISyntaxException, IOException, InterruptedException {
+    public void harness(HttpRequest request, Consumer<HttpResponse<String>> validate) throws URISyntaxException, IOException, InterruptedException {
         Thread t = new Thread(new Runnable() {
             public void run() {
-                Main.main(new String[]{});
+                try {
+                    Main.main(new String[]{});
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
         t.start();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:5000"))
-                .GET()
-                .build();
         HttpClient client = HttpClient.newHttpClient();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        assertEquals("Server should be up and running",200,response.statusCode());
+        validate.accept(response);
     }
+    public void testHealthCheck() throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://localhost:5000"))
+                .GET()
+                .build();
+        harness(request,response->assertEquals("Server should be up and running",200,response.statusCode()));
+    }
+    public void testLogin() throws URISyntaxException,IOException,InterruptedException{
+        String username="Thorsten";
+        String password = "Gruppenkacken_mit_Thomas12345";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://localhost:5000/login"))
+                .POST(HttpRequest.BodyPublishers.ofString(String.format("{\"username\":%s,\"password\":%s}",username,password)))
+                .build();
+        harness(request,response->assertEquals("Server should be up and running",200,response.statusCode()));
+    }
+
 }
